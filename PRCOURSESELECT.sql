8792349cd8435ecdb35d62d3ede3bb488f49a4ce
Arunkumar787238 <arun787238@gmail.com>
2024-12-03 19:33:56 +0530

    Add files via upload
    SQL 5 Stored Procedure

---
+++ PRCOURSESELECT.sql
@@ -0,0 +1,28 @@
+CREATE PROCEDURE PRCOURSESElECT
+(
+    @FLDCOURSEID                BIGINT
+   ,@ROWUSERCODE				INT
+  
+)
+AS
+BEGIN
+      SELECT * FROM TBLCOURSE WHERE FLDCOURSEID=@FLDCOURSEID 
+	  END
+
+
+
+	  
+
+
+	  
+
+
+
+	  
+
+
+	  
+
+
+
+	  
